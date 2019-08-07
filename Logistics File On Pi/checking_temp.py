33e8b8ed6cc4878efdd0a2292b52863f9eb1306f
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-08-07 09:37:39 +0700

    Create checking_temp.py

---
+++ Logistics File On Pi/checking_temp.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+def measure_temp():
+        temp = os.popen("vcgencmd measure_temp").readline()
+        return (temp.replace("CPU temperature: ",""))
+
+def get_time():
+	dtime = os.popen("date").readline()
+	return (dtime.replace("Date time: ",""))
+
+while True:
+	print(get_time())
+        print(measure_temp())
+        time.sleep(1)
