0baecf3a5b3c3e739749db04e50404601cf6b06f
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-08-07 09:37:36 +0700

    Create checking_temp.js

---
+++ Logistics File On Pi/checking_temp.js
@@ -0,0 +1,9 @@
+const { exec } = require('child_process');
+function getData()
+{
+	exec('date',(err,out)=>{if(err){console.error(err)}else{console.log(out)}});
+	exec('vcgencmd measure_temp',(err,out)=>{if(err){console.error(err)}else{console.log(out)}});
+	console.log('\n');
+}
+console.log('Getting data from CPU ...');
+setInterval(getData,1000);
