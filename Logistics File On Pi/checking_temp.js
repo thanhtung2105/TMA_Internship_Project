ad39fa62170dd70f8e629c9cd7cd9d38d2d141ff
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-09-12 11:03:34 +0700

    Delete checking_temp.js

--- Logistics File On Pi/checking_temp.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-function getData()
-{
-	exec('date',(err,out)=>{if(err){console.error(err)}else{console.log(out)}});
-	exec('sudo python Seeed_AMG8833.py',(err,out)=>{if(err){console.error(err)}else{console.log(out)}});
-	console.log('\n');
-}
-console.log('Getting information ...');
-setInterval(getData,3000);
